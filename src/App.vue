6ec13d40b018f68c3e5e8b6ff1e35f3a43937782
mioe <mioe.boss@gmail.com>
2019-10-20 03:11:44 +0300

    add toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <template lang="pug">
-router-view
+#app
+  toolbar
+  router-view
 </template>
 
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
+import Toolbar from '@/components/toolbar.vue'
 
-@Component
+@Component({
+  components: {
+    Toolbar,
+  },
+})
 export default class App extends Vue {}
 </script>
---
+++ src/components/toolbar.vue
@@ -0,0 +1,48 @@
+<template lang="pug">
+.toolbar
+  .item.logo
+    logo/
+  .item
+    p edit
+</template>
+
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+import Logo from '@/components/logo.vue'
+
+@Component({
+  components: {
+    Logo,
+  },
+})
+export default class Toolbar extends Vue {}
+</script>
+
+
+<style scoped lang="sass">
+.toolbar
+  position: relative
+  width: 100%
+  height: 100%
+  background: #24292e
+  border-right: 2px solid #30373E
+
+.item
+  position: relative
+  width: 100%
+  height: 50px
+  cursor: pointer
+  &:hover
+    background: #282D33
+
+.logo
+  position: sticky
+  border-bottom: 2px solid #30373E
+  svg
+    position: absolute
+    width: 30px
+    top: 50%
+    left: 50%
+    transform: translate(-50%, -50%)
+</style>
